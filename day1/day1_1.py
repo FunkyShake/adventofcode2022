428f8c456f5a8c390705cdd1d0302baea74cdfe8
FunkyShake <75706045+FunkyShake@users.noreply.github.com>
2022-12-01 12:23:37 -0500

    Create day1_1.py

---
+++ day1/day1_1.py
@@ -0,0 +1,18 @@
+with open('input.txt', 'r') as f:
+  ls = [item.strip() for item in f]
+  
+  biggest = 0
+  adder = 0
+  
+  for i in ls:
+    if i != '':
+      adder += int(i)
+      
+    else:
+        if adder > biggest:
+            biggest = adder
+            adder = 0
+          else:
+            adder = 0
+  
+  print(biggest)
