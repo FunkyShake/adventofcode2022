6a07d684c6c97a3c3ea3e1303435a2c6bdd2658e
FunkyShake <75706045+FunkyShake@users.noreply.github.com>
2022-12-02 12:20:13 -0500

    Create day2_1.py

---
+++ day2/day2_1.py
@@ -0,0 +1,31 @@
+with open('input.txt', 'f') as f:
+  ls = [item.split(' ') for item in f.read().split('\n')]
+  
+  points = 0
+  
+  for play in ls:
+    if play[1] == 'X':
+      points += 1
+      if play[0] == 'C':
+        points += 6
+      elif play[0] == 'A':
+        points += 3
+      else:
+        pass
+      
+    if play[1] == 'Y':
+      points += 2
+      if play[0] == 'A':
+        points += 6
+      elif play[0] == 'B':
+        points += 3
+      else:
+        pass
+    else:
+      points += 3
+      if play[0] == 'B':
+        points += 6
+      elif play[0] == 'C':
+        points += 3
+      else:
+        pass
