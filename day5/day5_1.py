dbe39f0a894fbed8b21b42b914325afa14d9220e
FunkyShake <75706045+FunkyShake@users.noreply.github.com>
2022-12-05 13:15:30 -0500

    Update day5_1.py
    added day 2, one day i'll figure out this array stuff

--- day5/day5_1.py
+++ day5/day5_1.py
@@ -2,7 +2,15 @@ import re
 
 # i couldn't quite figure out how to make an array plus instructions
 # out of the input so here's a hand-jammed array
-data = [
+data1 = [
+  ['S', 'L', 'W'], ['J','T','N','Q'], ['S','C','H','F','J'], 
+  ['T','R','M','W','N','G','B'],['T','R','L','S','D','H','Q','B'],
+  ['M','J','B','V','F','H','R','L'], ['D','W','R','N','J','M'], 
+  ['B','Z','T','F','H','N','D','J'], ['H','L','Q','N','B','F','T']
+]
+
+# let's face it, it's a struggle for some of us
+data2 = [
   ['S', 'L', 'W'], ['J','T','N','Q'], ['S','C','H','F','J'], 
   ['T','R','M','W','N','G','B'],['T','R','L','S','D','H','Q','B'],
   ['M','J','B','V','F','H','R','L'], ['D','W','R','N','J','M'], 
@@ -15,6 +23,9 @@ with open('input.txt', 'r') as f:
   
   for i in ls:
     for times in range(1, int(i[0])+1):
-      data[int(i[2])-1].append(data[int(i[1])-1].pop())
+      data1[int(i[2])-1].append(data1[int(i[1])-1].pop())
+
+    data2[int(i[2])-1] = data2[int(i[2])-1] + data2[int(i[1])-1][-int(i[0]):]
+    data2[int(i[1])-1] = data2[int(i[1])-1][:-int(i[0])]
   
-  print(''.join([a[-1] for a in data]))
+  print(''.join([a[-1] for a in data1]), ''.join([b[-1] for b in data2]))
