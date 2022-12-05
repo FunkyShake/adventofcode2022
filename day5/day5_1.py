8f6d5cf647d41b27d23418c539e4da310ef39b36
FunkyShake <75706045+FunkyShake@users.noreply.github.com>
2022-12-05 12:38:32 -0500

    Create day5_1.py

---
+++ day5/day5_1.py
@@ -0,0 +1,20 @@
+import re
+
+# i couldn't quite figure out how to make an array plus instructions
+# out of the input so here's a hand-jammed array
+data = [
+  ['S', 'L', 'W'], ['J','T','N','Q'], ['S','C','H','F','J'], 
+  ['T','R','M','W','N','G','B'],['T','R','L','S','D','H','Q','B'],
+  ['M','J','B','V','F','H','R','L'], ['D','W','R','N','J','M'], 
+  ['B','Z','T','F','H','N','D','J'], ['H','L','Q','N','B','F','T']
+]
+
+with open('input.txt', 'r') as f:
+  
+  ls = [re.findall(r'\d+', l) for l in f.readlines()]
+  
+  for i in ls:
+    for times in range(1, int(i[0])+1):
+      data[int(i[2])-1].append(data[int(i[1])-1].pop())
+  
+  print(''.join([a[-1] for a in data]))
