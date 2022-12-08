8504c97494b7ee01e62a5413c530c4791c3439ec
FunkyShake <75706045+FunkyShake@users.noreply.github.com>
2022-12-08 18:48:59 -0500

    Update d8.py
    fixed issues

--- day8/d8.py
+++ day8/d8.py
@@ -1,44 +1,43 @@
-#TODO: figure out why it works for test 
-# and why np.array reverse x,y in diff interpreters
-
 import numpy as np
 
 def north(grid, pos_x, pos_y):
-  n = [grid[i, pos_y] for i in reversed(range(pos_x))]
-  return any(grid[pos_x, pos_y] <= item for item in n)
-  
+    n = [grid[i, pos_y] for i in reversed(range(pos_x))]
+    return any(grid[pos_x, pos_y] <= item for item in n)
+
 def south(grid, pos_x, pos_y):
-  s = [grid[i, pos_y] for i in range(pos_x+1)]
-  return any(grid[pos_x, pos_y] <= item for item in s)
-  
+    s = [grid[i, pos_y] for i in range(pos_x + 1, len(grid))]
+    return any(grid[pos_x, pos_y] <= item for item in s)
+
 def west(grid, pos_x, pos_y):
-  w = [grid[pos_x, i] for i in reversed(range(pos_y))]
-  return any(grid[pos_x, pos_y] <= item for item in w)
+    w = [grid[pos_x, i] for i in reversed(range(pos_y))]
+    return any(grid[pos_x, pos_y] <= item for item in w)
 
 def east(grid, pos_x, pos_y):
-  e = [grid[pos_x, i] for i in range(pos_y+1)]
-  return any(grid[pos_x, pos_y] <= item for item in e)
+    e = [grid[pos_x, i] for i in range(pos_y + 1, len(grid))]
+    print(e)
+    return any(grid[pos_x, pos_y] <= item for item in e)
 
-with open('test.txt', 'r') as f:
-  arr = np.array([[int(letter) for letter in list(l)] for l in f.read().split('\n')])
-  
-  visible = (len(arr) * len(arr))
-    
-  for x in range(1, len(arr)-1):
-    for y in range(1, len(arr)-1):
 
-      if north(arr, x, y):
-        if south(arr, x, y):
-          if west(arr, x, y):
-            if east(arr, x, y):
-              visible -= 1
+with open('input.txt', 'r') as f:
+    arr = np.array([[int(letter) for letter in list(l)] for l in f.read().split('\n')])
+
+    visible = (len(arr) * len(arr))
+
+    for x in range(1, len(arr) - 1):
+        for y in range(1, len(arr) - 1):
+
+            if north(arr, x, y):
+                if south(arr, x, y):
+                    if west(arr, x, y):
+                        if east(arr, x, y):
+                            visible -= 1
+                        else:
+                            continue
+                    else:
+                        continue
+                else:
+                    continue
             else:
-              break
-          else:
-            break
-        else:
-          break
-      else:
-        break
-          
-  print(visible)
+                continue
+
+    print(visible)
