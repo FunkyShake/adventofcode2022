9d0c86d7cbe8e3e286463fd8d3f099816408be6f
FunkyShake <75706045+FunkyShake@users.noreply.github.com>
2022-12-10 20:55:23 -0500

    Update d9_9.py
    test input works, puzzle input does not; can't wrap my head around logic for this one

--- day9/d9_9.py
+++ day9/d9_9.py
@@ -13,29 +13,47 @@ def movement(d, m, y, x):
 
     return y, x
 
-with open('test.txt', 'r') as f:
+with open('input.txt', 'r') as f:
     data = [l.split(' ') for l in f.read().split('\n')]
-
-    visited = []
-    prev_moves = []
+    visited = [(0,0)]
 
     y1, x1, y2, x2, = 0, 0, 0, 0
 
     for ipt in data:
         d, m = ipt[0], int(ipt[1])
-        if m < 2:  # single movement will never be same as prev direction
+        # only really need to track the head
+        if m < 2:
             y1, x1 = movement(d, m, y1, x1)
-
         else:
             for i in range(m):
-                if i == 0:
-                    y1, x1 = movement(d, m, y1, x1)
-                    # if it's movement is still within 1 of x2, y2
-
-                else:
-                    y1, x1 = movement(d, m, y1, x1)
-                    y2, x2 = movement(d, m, y2, x2)
-
-                    visited.append((y2,x2))
-
-print(visited)
+                # if the head moves more than two spaces from last point
+                if d == 'U':
+                    if len(range(y1, y2)) == 1:
+                        y2, x2 = y1, x1
+                        visited.append((y2, x2))
+                        y1, x1 = movement(d, m, y1, x1)
+                    else:
+                        y1, x1 = movement(d, m, y1, x1)
+                elif d == 'D':
+                    if len(range(y2, y1)) == 1:
+                        y2, x2 = y1, x1
+                        visited.append((y2, x2))
+                        y1, x1 = movement(d, m, y1, x1)
+                    else:
+                        y1, x1 = movement(d, m, y1, x1)
+                elif d == 'L':
+                    if len(range(x1, x2)) == 1:
+                        y2, x2 = y1, x1
+                        visited.append((y2, x2))
+                        y1, x1 = movement(d, m, y1, x1)
+                    else:
+                        y1, x1 = movement(d, m, y1, x1)
+                elif d == 'R':
+                    if len(range(x2, x1)) == 1:
+                        y2, x2 = y1, x1
+                        visited.append((y2, x2))
+                        y1, x1 = movement(d, m, y1, x1)
+                    else:
+                        y1, x1 = movement(d, m, y1, x1)
+                # then second point should become that last point
+    print(len(set(visited)))
